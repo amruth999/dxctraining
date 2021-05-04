3c82cea36ccd99cb7cd10d62890e7a575958d160
amruth999 <36686928+amruth999@users.noreply.github.com>
2021-05-04 22:18:07 +0530

    Create SampleSpringMvcApplicationTests.java

---
+++ Spring MVC/sampleSpringMVC/src/test/java/com/example/sampleSpringMVC/SampleSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleSpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
