9b449101110a10383c486c4d65284db5659dc13e
amruth999 <36686928+amruth999@users.noreply.github.com>
2021-05-04 22:08:22 +0530

    Create SampleSpringMvcApplication.java

---
+++ Spring MVC/sampleSpringMVC/src/main/java/com/example/sampleSpringMVC/SampleSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringMvcApplication.class, args);
+	}
+
+}
